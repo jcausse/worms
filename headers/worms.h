c6ff2984363ced05e18d12158a4c3ab0e3a05aa8
jcausse <jcausse@itba.edu.ar>
2021-03-31 13:16:42 -0300

    Comments added

--- headers/worms.h
+++ headers/worms.h
@@ -13,28 +13,29 @@
 class Worm {
 public:
 	//Constructor
-	Worm(float initialXPosition, float initialYPosition);
+	Worm(float initialXPosition, float initialYPosition);	//Explicit worm constructor with initial position
 	//int state;
 
 	//Data members
-	float x;
-	float y;
+	float x;				//X-axis position
+	float y;				//Y-axis position
 	int wormmoves;
 	int salto;
-	bool wormsteady;
+	bool wormsteady;		//Equals "true" if the worm is NOT moving
 	int salto_cooldown;
 	bool salto_lock;
-	bool wormright;
+	bool wormright;			//Equals "true" if the worm is facing right
 
-	void go_up(void);
+	//Movement methods
+	void go_up(void);		
 	void go_right(void);
 	void go_left(void);
 	void jumping(void);
 	void released_up(void);
 };
 
-void move_worms(Worm* worm1, Worm* worm2, Keys* key);
-int collidewborder(float x,float y);
+void move_worms(Worm* worm1, Worm* worm2, Keys* key);	
+int collidewborder(float x,float y);					//Check if the worm collides with a screen border
 int isjumping(float y);
 
 #endif // WORMS_H 
